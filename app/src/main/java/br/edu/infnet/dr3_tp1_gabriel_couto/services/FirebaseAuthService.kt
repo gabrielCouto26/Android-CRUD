5ec228f8bd0973654448bbd1aa4872b4b7b763f5
GabrielCouto <gabrielcouto26@gmail.com>
2021-03-01 19:30:06 -0300

    iniciando firebaseAuthService

--- app/src/main/java/br/edu/infnet/dr3_tp1_gabriel_couto/services/FirebaseAuthService.kt
+++ app/src/main/java/br/edu/infnet/dr3_tp1_gabriel_couto/services/FirebaseAuthService.kt
@@ -1,4 +1,20 @@
 package br.edu.infnet.dr3_tp1_gabriel_couto.services
 
-class FirebaseAuthService {
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
+class FirebaseAuthService() {
+    val firebaseAuth = FirebaseAuth.getInstance()
+    fun signIn(email: String, senha: String): Task<AuthResult> {
+        return firebaseAuth.signInWithEmailAndPassword(email, senha)
+    }
+
+    fun createUserWithEmailAndPassword(email: String, senha: String): Task<AuthResult> {
+        return firebaseAuth.createUserWithEmailAndPassword(email, senha)
+    }
+
+    fun isLoggedIn(): Boolean {
+        return firebaseAuth.currentUser != null
+    }
 }
