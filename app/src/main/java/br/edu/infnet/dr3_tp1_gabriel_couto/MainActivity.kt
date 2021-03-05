c206bd027a063cc0427e38268cb8decc43aa7a1b
GabrielCouto <gabrielcouto26@gmail.com>
2021-03-04 23:26:09 -0300

    implementando bottom navigation view

--- app/src/main/java/br/edu/infnet/dr3_tp1_gabriel_couto/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/dr3_tp1_gabriel_couto/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +14,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.navHostFragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.listaFuncionariosFragment, R.id.showFuncionarioFragment, R.id.formFuncionarioFragment))
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.listaFuncionariosFragment, R.id.formFuncionarioFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+
 
     }
 }
