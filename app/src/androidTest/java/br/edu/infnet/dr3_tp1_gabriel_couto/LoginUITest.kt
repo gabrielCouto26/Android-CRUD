860dcbae730693efc232266a4e062b95da350a94
GabrielCouto <gabrielcouto26@gmail.com>
2021-03-19 21:19:00 -0300

    iniciando testes

---
+++ app/src/androidTest/java/br/edu/infnet/dr3_tp1_gabriel_couto/LoginUITest.kt
@@ -0,0 +1,86 @@
+package br.edu.infnet.dr3_tp1_gabriel_couto
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class LoginUITest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun verificarTelaLogin(){
+        onView(withHint("Email"))
+            .check(matches(isDisplayed()))
+            .check(matches(isEnabled()))
+
+        onView(withHint("Senha"))
+            .check(matches(isDisplayed()))
+            .check(matches(isEnabled()))
+
+        onView(withId(R.id.btnEntrar))
+            .check(matches(isDisplayed()))
+            .check(matches(isEnabled()))
+
+        onView(withId(R.id.btnRedirectCadastro))
+            .check(matches(isDisplayed()))
+            .check(matches(isEnabled()))
+
+    }
+
+    @Test
+    fun verificaSeNaoPermiteLogarComCamposEmBranco(){
+        onView(withHint("Email"))
+            .perform(clearText())
+
+        onView(withHint("Senha"))
+            .perform(clearText(), closeSoftKeyboard())
+
+        onView(withId(R.id.btnEntrar))
+            .perform(click())
+
+        onView(withText("Email ou senha inválidos"))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun verificarSeUsuarioValidoConsegueLogar(){
+        onView(withHint("Email"))
+            .perform(typeText("gabriel@email.com"))
+
+        onView(withHint("Senha"))
+            .perform(typeText("123456"), closeSoftKeyboard())
+
+        onView(withId(R.id.btnEntrar))
+            .perform(click())
+
+        onView(withId(R.id.listaFuncionarios))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun verificaSeUsuarioInvalidoSeLoga(){
+        onView(withHint("Email"))
+            .perform(typeText("gabriel@email.com"))
+
+        onView(withHint("Senha"))
+            .perform(typeText("123456"), closeSoftKeyboard())
+
+        onView(withId(R.id.btnEntrar))
+            .perform(click())
+
+        onView(withId(R.id.listaFuncionarios))
+            .check(matches(isDisplayed()))
+    }
+
+}
--- app/src/main/java/br/edu/infnet/dr3_tp1_gabriel_couto/services/api/viaCepApi.kt
+++ app/src/main/java/br/edu/infnet/dr3_tp1_gabriel_couto/services/api/viaCepApi.kt
@@ -18,5 +18,4 @@ object viaCepApi {
     }
 
     fun getViaCepApiService(): ViaCepApiService = getInstance().create(ViaCepApiService::class.java)
-
 }
